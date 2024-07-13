38e514b3f2ddb790db51e6b2858ee2d07f3cd14f
DCTP-BE <akpata.thelma@gmail.com>
2024-07-13 17:35:52 +0100

    v1

--- src/bookControllers/booksController.js
+++ src/bookControllers/booksController.js
@@ -2,35 +2,51 @@ const express = require('express');
 const app = express();
 const books = require('../database/booksData.json');
 
-
+app.use(express.json());
 
 const bookController = {
-async = await bookController {
-    app.get('/books/:id', (req, res) => {
-        // Return a single book by ID
-        const bookId = req.params.id;
-        res.json({
-            id: bookId, title: `Book ${bookId}`, author: `Author ${bookId}`
-        });
-    });
-    
-    app.post('/books', (req, res) => {
+    async createBook (req, res) {
         // Create a new book
-        const book = req.body;
-        res.json({ id: 3, title: book.title, author: book.author });
-    });
-    
-    app.put('/books/:id', (req, res) => {
-        // Update a book
+        const newBook = req.body;
+        res.json({ id: books.length + 1, 
+            title: newBook.title, 
+            author: newBook.author });
+    }
+    async getBookById(req, res) {
         const bookId = req.params.id;
-        const book = req.body;
-        res.json({ id: bookId, title: book.title, author: book.author });
-    });
-    
-    app.delete('/books/:id', (req, res) => {
-        // Delete a book
-        const bookId = req.params.id;
-        res.json({ message: `Book ${bookId} deleted` });
-    });
+        const book = books.find((book) => book.id === parseInt(bookId));
+        if (!book) {
+          res.status(404).json({ message: `Book not found` });
+        } else {
+          res.json(book);
+        }
+
+        
+        app.get('/books/:id', (req, res) => {
+            // Return a single book by ID
+            const bookId = req.params.id;
+            res.json({
+                id: bookId, title: `Book ${bookId}`, author: `Author ${bookId}`
+            });
+        });
+
+app.post('/books', (req, res) => {
+    // Create a new book
+    const book = req.body;
+    res.json({ id: 3, title: book.title, author: book.author });
+});
+
+app.put('/books/:id', (req, res) => {
+    // Update a book
+    const bookId = req.params.id;
+    const book = req.body;
+    res.json({ id: bookId, title: book.title, author: book.author });
+});
+
+app.delete('/books/:id', (req, res) => {
+    // Delete a book
+    const bookId = req.params.id;
+    res.json({ message: `Book ${bookId} deleted` });
+});
     
 }
